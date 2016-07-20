a4acf9d5fd1126428b36decfbaf048d969c81a7c
dmc12345628 <dmc12345628@gmail.com>
2016-07-20 13:10:02 -0700

    Add files via upload

---
+++ WPF/Numericboard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SelfCheckOut
+{
+    /// <summary>
+    /// Interaction logic for Numericboard.xaml
+    /// </summary>
+    public partial class Numericboard : Window
+    {
+        public Numericboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
